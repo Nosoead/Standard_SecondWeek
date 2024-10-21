285bcd5e409429039711060b74a4c390f7a8d4e9
Nosoead <thseodh@gmail.com>
2024-10-21 12:29:47 +0900

    [Feature]_Scripts_Son // [Complete]_Q1-2
    QuestManager에 Instance getter/setter 만들고
    조건에 따라 QuestManager를  찾거나, 생성해서 AddComponent하게 했습니다.

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -7,5 +7,22 @@ public class QuestManager : MonoBehaviour
 
     private static QuestManager instance;
 
+    public QuestManager Instance
+    {
+        get { return instance; }
+        set
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+                if (instance == null)
+                {
+                    GameObject QuestManager = new GameObject();
+                    QuestManager.AddComponent<QuestManager>();
+                }
+            }
+        }
+    }
+
 
 }
